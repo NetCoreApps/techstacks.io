acbdbf09694e93e53283076c3bfd523a1f5dc368
Demis Bellot <demis.bellot@gmail.com>
2026-07-23 16:24:14 +0800

    fix tags on featured post

--- TechStacks.Client/src/app/page.tsx
+++ TechStacks.Client/src/app/page.tsx
@@ -370,7 +370,7 @@ function FeaturedStory({
           </div>
         </div>
 
-        <div className="mt-4 pt-4 border-t border-gray-100 flex items-center justify-between">
+        <div className="mt-4 pt-4 border-t border-gray-100 space-y-3">
           <div className="flex items-center gap-3.5">
             <VoteWidget
               postId={post.id!}
